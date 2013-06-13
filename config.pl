aa998bfe9107566ede6448b31e4da43fd8cb1b62
Christian Jaeger <chrjae@gmail.com>
2013-06-13 16:38:20 +0100

    add channels feature

--- config.pl
+++ config.pl
@@ -1,10 +1,16 @@
+my $channels= +{map {$_=>1} qw(FreeNode-#perl FreeNode-#debian)};
 {
  yournick=>
  {
   to=> 'you@example1743829.com'
+  # all logged channels
  },
  othernick=>
  {
-  to=> '...',
+  to=> '...@...',
+  # only notify about messages in given channels (hashref, channel
+  # including server name as in the logfile written by xchat, but
+  # without the .log suffix)
+  channels=> $channels,
  }
 }
